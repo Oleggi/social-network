621c9abe9e7bab73c3f5d0545592f79c52a8039f
Oleg Borysko <49756993+Oleggi@users.noreply.github.com>
2020-08-12 23:01:38 +0300

    Updates

---
+++ precache-manifest.f789ecc0ce4564039cb3346e99362ede.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d476683072195e0d0ef86f3c9cf0b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4b471684ab48b193a9",
+    "url": "/static/css/3.2f89832a.chunk.css"
+  },
+  {
+    "revision": "44c1866989e9cbc03f02",
+    "url": "/static/css/main.1d4dd1cb.chunk.css"
+  },
+  {
+    "revision": "b8f0f9c9583c7fe8d30a",
+    "url": "/static/js/2.b7293b82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b7293b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4b471684ab48b193a9",
+    "url": "/static/js/3.901fc802.chunk.js"
+  },
+  {
+    "revision": "6e65717d2073a65cb2e6",
+    "url": "/static/js/4.d7e37283.chunk.js"
+  },
+  {
+    "revision": "08250eb018f45cf7b8d4",
+    "url": "/static/js/5.2c3f3b4f.chunk.js"
+  },
+  {
+    "revision": "bef7f093118b2b7194e0",
+    "url": "/static/js/6.516a1460.chunk.js"
+  },
+  {
+    "revision": "44c1866989e9cbc03f02",
+    "url": "/static/js/main.d49c5ec2.chunk.js"
+  },
+  {
+    "revision": "560d3bb3593675111ee3",
+    "url": "/static/js/runtime-main.42eb3128.js"
+  }
+]);
---
+++ static/js/5.2c3f3b4f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-1"]=this["webpackJsonpreact-1"]||[]).push([[5],{296:function(e,t,n){"use strict";n.r(t);var a=n(8),r=n(9),c=n(11),u=n(10),i=n(0),s=n.n(i),l=function(e){Object(c.a)(n,e);var t=Object(u.a)(n);function n(){return Object(a.a)(this,n),t.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return s.a.createElement("div",null,s.a.createElement("h2",null,"News"))}}]),n}(s.a.Component);t.default=l}}]);
+//# sourceMappingURL=5.2c3f3b4f.chunk.js.map
